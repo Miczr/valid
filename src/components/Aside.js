b793f0ea2df3dc40b7c81c8f5449165d41a43a3b
Mickael Le-Huu-Nghia <M.LeHuuNghia@gmail.com>
2020-06-05 14:18:14 +0200

    modif/css

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Aside(){
     return(
         <aside className="aside">
-
+            <img className="img" src="image/Skate.jpg"/>
         </aside>
     )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Section(){
     return(
         <section className="section">
-            
+            <article className="article">OK</article>
+            <article className="article">IT'S OK</article>
         </section>
     )
 }
