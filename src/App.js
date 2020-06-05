e9d32ba79849728cdbcf13b21003cac0d9c9a01f
Mickael Le-Huu-Nghia <M.LeHuuNghia@gmail.com>
2020-06-05 12:31:15 +0200

    adding

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+
+    </div>
+  );
+}
+
+export default App;
